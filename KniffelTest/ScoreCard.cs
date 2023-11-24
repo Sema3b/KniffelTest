127d683b57263d906939ceef68e61d5a29c65eb6
Sema <semanur@threebit.io>
2023-11-24 09:16:06 +0100

    .

--- KniffelTest/ScoreCard.cs
+++ KniffelTest/ScoreCard.cs
@@ -1,6 +1,6 @@
 ﻿public class ScoreCard 
 {
-    private Dictionary<string, int> Scores { get; set; } // Dictionary wird als lokale Variable(?) initiallisiert, Aufbau verstehen.
+    private Dictionary<string, int> Scores { get; set; } // Dictionary wird als lokale Variable(?) initiallisiert, Aufbau verstehen..
     public int TotalScore => Scores.Values.Sum(); // Gesamtsumme > Summe Wurfwert
 
     public ScoreCard() // 3. Methode.
